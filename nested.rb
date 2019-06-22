87622a520ed73e631d1126d1b539d1051462ce92
Katana Tran <tkatyk21@gmail.com>
2019-06-22 00:47:36 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,17 +57,18 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+# 
 
 	programmer_hash = 
- 		{
+ 		{:yukihiro_matsumoto => {
+     :known_for => "Ruby",
+     :languages => ["LISP", "C"]
+ },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -79,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash
     
 end
 
@@ -102,6 +105,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+programmer_hash[:alan_kay][:known_for] = alans_new_info
+
+programmer_hash
      
 end
 
@@ -121,5 +127,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+programmer_hash[:dennis_ritchie][:languages][1]= "Assembly"
 
+programmer_hash
 end
